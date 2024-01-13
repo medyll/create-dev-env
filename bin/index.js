2d739a88ac6a9b3a3bf08539386640c79887f063
medyll <43032494+medyll@users.noreply.github.com>
2024-01-13 16:32:57 +0100

    fix: Add stdio option to execSync

--- bin/index.js
+++ bin/index.js
@@ -4,5 +4,5 @@ import { execSync } from "child_process";
 
 console.log("Hello @medyll");
 const args = (process.argv).slice(2).join(' ')
-execSync(`npx @medyll/dev-env ${args}`);
+execSync(`npx @medyll/dev-env ${args}`, { stdio: 'inherit' });
 console.log("Bye @medyll");
