516f9036cd9485552f234f6d4c2a14bdab07997c
medyll <43032494+medyll@users.noreply.github.com>
2024-01-13 16:29:40 +0100

    fix: Add import statement and additional console.log message

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env  node
 
+import { execSync } from "child_process";
+
 console.log("Hello @medyll");
 const args = (process.argv).slice(2).join(' ')
 execSync(`npx @medyll/dev-env ${args}`);
+console.log("Bye @medyll");
